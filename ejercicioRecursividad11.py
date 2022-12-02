df8aad2b649528da3de651f43b794beadcdb4a82
KLKJOSEANTONIO <kikoamamoros@gmail.com>
2022-12-02 14:21:39 +0100

    LÑ

--- ejercicioRecursividad11.py
+++ ejercicioRecursividad11.py
@@ -3,6 +3,12 @@ def palindromo(n):
     if len(n)==0:
         return n
     volteada = n[-1]+palindromo(n[:-1])
-    return n == volteada
+    if len(volteada) == len(n) and len(n)!=0:
+        if volteada == n:
+            return True
+        else:
+            return n
+    else:
+        return n
 n = "libra"
 print(palindromo(n))
