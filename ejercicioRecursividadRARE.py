ab4cbae479870b3a09c5f2fdc8c974788e0d6b4a
KLKJOSEANTONIO <kikoamamoros@gmail.com>
2022-12-02 09:56:54 +0100

    Create ejercicioRecursividadRARE.py

---
+++ ejercicioRecursividadRARE.py
@@ -0,0 +1,17 @@
+def contarLista(lista):
+    if len(lista)<=0:
+        return 0
+    return contarLista(lista[1:]) + lista[0]
+    
+lista = []
+while True:
+    num = input("Introduzca numero @ para parar: ")
+    if num == "@":
+        break
+    elif num !="@":
+        lista.append(int(num))
+suma = int(input("Suma de la lista?: "))
+if contarLista(lista) == suma:
+    print("La suma es correcta...")
+else:
+    print("La suma NO se corresponde...")
