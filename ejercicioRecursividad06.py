690200c098eabf417adb389924c54adbf5161fc1
KLKJOSEANTONIO <kikoamamoros@gmail.com>
2022-12-02 12:38:03 +0100

    Create ejercicioRecursividad06.py

---
+++ ejercicioRecursividad06.py
@@ -0,0 +1,11 @@
+#maximo lista
+def maximoLista(lista):
+    if len(lista) ==1:
+        return lista[0]
+    siguiente = maximoLista(lista[1:])
+    if lista[0]>=siguiente:
+        return lista[0]
+    else:
+        return siguiente
+lista = [2,9,3,45,17,4]
+print(maximoLista(lista))
